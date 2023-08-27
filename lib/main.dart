5b5fd62506e71421e0a677575587319e818d0363
RonnyAmoresAG <ronny.amores@outlook.com>
2023-08-27 16:20:06 -0500

    arreglo

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         ],
         child: MaterialApp(
           title: 'Pokedex',
-          initialRoute: '/',
+          initialRoute: '/login',
           routes: {
             '/': (context) => MainWidget(),
             '/pokemon-details': (context) => PokemonDetailsScreen(),
